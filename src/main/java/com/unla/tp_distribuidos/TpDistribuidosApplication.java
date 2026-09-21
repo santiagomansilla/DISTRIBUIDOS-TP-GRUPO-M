f7579ba8491b1beb0060c05c528cf2997a3652d3
santiagomansilla <santiagomansilla99@hotmail.com>
2026-09-21 16:00:22 -0300

    update

---
+++ src/main/java/com/unla/tp_distribuidos/TpDistribuidosApplication.java
@@ -0,0 +1,15 @@
+package com.unla.tp_distribuidos;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class TpDistribuidosApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(TpDistribuidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/tp_destribuidos/TpDistribuidosApplicationTest.java
@@ -0,0 +1,13 @@
+package com.unla.tp_destribuidos;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class TpDistribuidosApplicationTest {
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
